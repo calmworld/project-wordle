3614050182c392ddcdb87e4377d83502dbdd6923
calmworld <sarah.alhuriz@hotmail.com>
2024-07-29 10:18:24 -0600

    winning and losing

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function GuessInput({ handleSubmitGuess }) {
+function GuessInput({ gameStatus, handleSubmitGuess }) {
     const [tentativeGuess, setTentativeGuess] = React.useState('');
 
   function handleSubmit(event) {
@@ -20,6 +20,7 @@ function GuessInput({ handleSubmitGuess }) {
         maxLength={5}
         pattern="[a-zA-Z]{5}"
         title="5 letter word"
+        disabled={gameStatus !== 'running'}
         value={tentativeGuess}
         onChange={(event) => {
           const nextGuess = event.target.value.toUpperCase();
