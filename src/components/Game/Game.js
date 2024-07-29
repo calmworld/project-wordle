ad2c01db3ba4aabc80332c753894e3860b3657aa
calmworld <sarah.alhuriz@hotmail.com>
2024-07-29 09:58:33 -0600

    keeping track of guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,15 +3,22 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess]);
+  }
   return (
     <>
-      <GuessInput />
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
 }
